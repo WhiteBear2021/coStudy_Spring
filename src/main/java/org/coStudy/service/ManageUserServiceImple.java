db5fc65433ce7b1e8913dce0a03c0ab5eee9fb1f
suhyu <suhyu@DESKTOP-9LQJ3BA>
2021-05-12 14:20:34 +0900

    관리자 - 사용자 리스트 , 새로운 사용자 리스트 끝

--- src/main/java/org/coStudy/service/ManageUserServiceImple.java
+++ src/main/java/org/coStudy/service/ManageUserServiceImple.java
@@ -1,8 +1,26 @@
 package org.coStudy.service;
 
+import java.util.List;
+
+import org.coStudy.domain.UserVO;
+import org.coStudy.mapper.UserMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ManageUserServiceImple implements ManagerUserService{
+
+	@Autowired
+	UserMapper mapper;
+	
+	@Override
+	public List<UserVO> list() {
+		return mapper.userInfo();
+	}
+
+	@Override
+	public List<UserVO> newList(String dateString) {
+		return mapper.newUserInfo(dateString);
+	}
 
 }
