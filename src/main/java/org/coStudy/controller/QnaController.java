62676cb2a0ad515be9ba29b899307431d74e7bcf
WhiteBear2021 <hyunwoo921129@gmail.com>
2021-05-17 17:38:23 +0900

    merge1 commit

--- src/main/java/org/coStudy/controller/QnaController.java
+++ src/main/java/org/coStudy/controller/QnaController.java
@@ -6,7 +6,6 @@ import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.coStudy.domain.Jamong;
 import org.coStudy.domain.QnaVO;
 import org.coStudy.service.QnaService;
 import org.json.simple.JSONArray;
@@ -36,54 +35,54 @@ import lombok.extern.log4j.Log4j;
 @RequestMapping("/qna/*")
 @AllArgsConstructor
 public class QnaController {
-	private QnaService service;
+   private QnaService service;
 
-/*	@PostMapping(value = "/user_insert", consumes = "application/json", produces = { MediaType.TEXT_PLAIN_VALUE })
-	public ResponseEntity<String> create(@RequestBody QnaVO qna) {
-		log.info("qnavo:" + qna);
-		log.info("===================");
-		int insertCount = service.Userinsert(qna);
+/*   @PostMapping(value = "/user_insert", consumes = "application/json", produces = { MediaType.TEXT_PLAIN_VALUE })
+   public ResponseEntity<String> create(@RequestBody QnaVO qna) {
+      log.info("qnavo:" + qna);
+      log.info("===================");
+      int insertCount = service.Userinsert(qna);
 
-		return insertCount == 1 ? new ResponseEntity<>("success", HttpStatus.OK)
-				: new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+      return insertCount == 1 ? new ResponseEntity<>("success", HttpStatus.OK)
+            : new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
 
-	}*/
-/*	   @RequestMapping(value="/user_insert", method=RequestMethod.POST)
-	   @ResponseBody
-	public String serialize(Jamong jamong) {
+   }*/
+/*      @RequestMapping(value="/user_insert", method=RequestMethod.POST)
+      @ResponseBody
+   public String serialize(Jamong jamong) {
         //필요한 로직 처리   
-		   log.info(jamong.getName());
+         log.info(jamong.getName());
         return jamong.getName() + jamong.getAge();
     }
-	
-	
-	@GetMapping(value = "/user_insert")
-	public void insert() {
+   
+   
+   @GetMapping(value = "/user_insert")
+   public void insert() {
 
-	}*/
-	
-	@GetMapping(value="/user_insert")
-	public void insert() {
+   }*/
+   
+   @GetMapping(value="/user_insert")
+   public void insert() {
 
-	}
-	
-	@GetMapping(value="/list")
-	public void list() {
+   }
+   
+   @GetMapping(value="/list")
+   public void list() {
 
-	}
-	
-	
-	@PostMapping(value="/user_insert")
+   }
+   
+   
+   @PostMapping(value="/user_insert")
     @ResponseBody
     public String ajax_addComment(@ModelAttribute("qnaVO") QnaVO qnaVO) throws Exception{
         QnaVO qna=new QnaVO();
       
         
         try{
-        	qna.setQna_content(qnaVO.getQna_content());
-        	qna.setQna_title(qnaVO.getQna_title());
-        	qna.setUser_no(qnaVO.getUser_no());
-        	service.Userinsert(qna);
+           qna.setQna_content(qnaVO.getQna_content());
+           qna.setQna_title(qnaVO.getQna_title());
+           qna.setUser_no(qnaVO.getUser_no());
+           service.Userinsert(qna);
             
         } catch (Exception e){
             e.printStackTrace();
@@ -91,35 +90,35 @@ public class QnaController {
         
         return "success";
     }
-	
-	 @RequestMapping(value="/commentList", 
-			 produces="application/json; charset=utf8")
-	    @ResponseBody
-	    public ArrayList<HashMap> ajax_commentList(@ModelAttribute("qnaVO") QnaVO qnaVO) throws Exception{
-	           
-		 ArrayList<HashMap> hmlist = new ArrayList<HashMap>();
+   
+    @RequestMapping(value="/commentList", 
+          produces="application/json; charset=utf8")
+       @ResponseBody
+       public ArrayList<HashMap> ajax_commentList(@ModelAttribute("qnaVO") QnaVO qnaVO) throws Exception{
+              
+       ArrayList<HashMap> hmlist = new ArrayList<HashMap>();
 
-		 
-		 	int user_no=qnaVO.getUser_no();
-		 	log.info("========================================");
-		 log.info(user_no);
-	        // 해당 게시물 댓글
-	        List<QnaVO> commentVO =service.UserList(user_no);
-	        
-	        if(commentVO.size() > 0){
-	            for(int i=0; i<commentVO.size(); i++){
-  	
-	                HashMap hm = new HashMap();
-	                hm.put("qna_title", commentVO.get(i).getQna_title());
-	                hm.put("qna_content", commentVO.get(i).getQna_content());
-	                
-	                hmlist.add(hm);
+       
+          int user_no=qnaVO.getUser_no();
+          log.info("========================================");
+       log.info(user_no);
+           // 해당 게시물 댓글
+           List<QnaVO> commentVO =service.UserList(user_no);
+           
+           if(commentVO.size() > 0){
+               for(int i=0; i<commentVO.size(); i++){
+     
+                   HashMap hm = new HashMap();
+                   hm.put("qna_title", commentVO.get(i).getQna_title());
+                   hm.put("qna_content", commentVO.get(i).getQna_content());
+                   
+                   hmlist.add(hm);
 
-	            }
-	            
-	        }
+               }
+               
+           }
   
-	        return hmlist;
-	        
-	    }
+           return hmlist;
+           
+       }
 }
