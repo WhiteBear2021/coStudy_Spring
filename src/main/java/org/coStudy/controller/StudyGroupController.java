9335e6bb04ca00bd361517470ee2fc520b0fab6a
82105 <82105@LAPTOP-2A3JGJ3H>
2021-05-20 17:49:44 +0900

    stduygroup 목록,디테일,만들기 수정

--- src/main/java/org/coStudy/controller/StudyGroupController.java
+++ src/main/java/org/coStudy/controller/StudyGroupController.java
@@ -1,5 +1,76 @@
 package org.coStudy.controller;
 
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.request;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.inject.Inject;
+
+import org.coStudy.domain.Search;
+import org.coStudy.domain.StudyGroupVO;
+import org.coStudy.service.StudyGroupService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.log4j.Log4j;
+
+@Controller
+@Log4j
+@RequestMapping("/studyGroup/*")
+@AllArgsConstructor
 public class StudyGroupController {
+	
+	private StudyGroupService service;
+
+	@RequestMapping(value = "/list" , method = RequestMethod.GET)
+	public String list(Model model
+			,@RequestParam(required = false, defaultValue = "category_no")String searchType
+			,@RequestParam(required = false)String keyword
+	) throws Exception {
+		
+		Search search = new Search();
+		search.setSearchType(searchType);
+		search.setKeyword(keyword);
+		
+		
+		model.addAttribute("list", service.list(search));
+		return "studyGroup/list";
+	}
+
+	@PostMapping("/insert")
+	public String insert(StudyGroupVO studygroup, RedirectAttributes rttr) throws Exception {
+		log.info("========================================");
+		service.insert(studygroup);
+		
+		Map<Object, Object> map=new HashMap<>();
+		map.put("success_no", studygroup.getStudygroup_no());
+		map.put("page", "insert");
+		
+		rttr.addFlashAttribute("success", map);
+		
+		return "redirect:/studyGroup/list";
+	}
+	
+	
+	@GetMapping("/insert")
+	public void insert(){
+		
+	}
+	
+
+	@GetMapping("/detail")
+	public void datail(@RequestParam("studygroup_no") int studygroup_no, Model model) {
+		model.addAttribute("studygroup", service.detail(studygroup_no));
+	}
+	
+	
 
 }
--- src/main/java/org/coStudy/service/StudyGroupServiceImple.java
+++ src/main/java/org/coStudy/service/StudyGroupServiceImple.java
@@ -1,8 +1,55 @@
 package org.coStudy.service;
 
+import java.util.List;
+
+import javax.inject.Inject;
+
+import org.apache.ibatis.session.RowBounds;
+import org.coStudy.domain.ApplyGroupMemberVO;
+import org.coStudy.domain.Search;
+import org.coStudy.domain.StudyGroupVO;
+import org.coStudy.mapper.StudyGroupMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-@Service
-public class StudyGroupServiceImple implements StudyGroupService{
+import lombok.AllArgsConstructor;
 
+@Service
+@AllArgsConstructor
+public class StudyGroupServiceImple implements StudyGroupService{
+	@Inject
+	private StudyGroupMapper mapper;
+	
+	@Override
+	public List<StudyGroupVO> list(Search search) throws Exception {
+		return mapper.list(search);
+	}
+	
+	@Override
+	public void insert(StudyGroupVO studygroup) throws Exception {
+		mapper.insert(studygroup);
+	}
+	
+	@Override
+	public StudyGroupVO detail(int studygroup_no) {
+		return mapper.detail(studygroup_no);
+	}
+
+	/*@Override
+	public int countstudyGroupList() {
+		return mapper.countstudyGroupList();
+	}
+
+	@Override
+	public int insertApplyGroupStudy(ApplyGroupMemberVO applyGroupMember) {
+		return mapper.insertApplyGroupStudy(applyGroupMember);
+	}*/
+
+
+	/*@Override
+	public int getGroupNo(String studygroup_name) {
+		return mapper.getGroupNo(studygroup_name);
+	}*/
+
+	
 }
