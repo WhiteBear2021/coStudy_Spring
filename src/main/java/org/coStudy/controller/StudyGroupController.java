488f9647078862223c70d0db8a4423ed45dbc128
WhiteBear2021 <hyunwoo921129@gmail.com>
2021-05-27 19:03:41 +0900

    upload path 수정 commit 그룹

--- src/main/java/org/coStudy/controller/StudyGroupController.java
+++ src/main/java/org/coStudy/controller/StudyGroupController.java
@@ -36,6 +36,9 @@ public class StudyGroupController {
 
 	private StudyGroupService service;
 
+//	@Resource(name = "uploadPath")
+//	private String uploadPath;
+
 	@GetMapping(value = "/list")
 	public void list(Criteria cri, Model model) {
 		log.info(cri);
@@ -66,21 +69,21 @@ public class StudyGroupController {
 
 	@PostMapping("/insert")
 	public String insert(HttpSession session,StudyGroupVO studygroup, RedirectAttributes rttr, MultipartFile file) throws Exception {
-		 String root_path = session.getServletContext().getRealPath("/");  
-	     String uploadPath = "resources/";
-	     String filename = file.getOriginalFilename();
-		String imgUploadPath = uploadPath + File.separator + "imgUpload";
-		String ymdPath = UploadFileUtils.calcPath(imgUploadPath);
-		String fileName = null;
-		UUID uuid = UUID.randomUUID();
-		 log.info("root path:"+root_path);
-	     log.info("upload path:"+uploadPath);
-	     log.info("file name:"+filename);
+		String root_path = session.getServletContext().getRealPath("/");  
+	      String uploadPath = "resources/";
+	      String filename = file.getOriginalFilename();
+	      log.info("root path:"+root_path);
+	      log.info("upload path:"+uploadPath);
+	      log.info("file name:"+filename);
+	      String imgUploadPath = root_path+uploadPath + File.separator + "imgUpload";
+	      String ymdPath = UploadFileUtils.calcPath(imgUploadPath);
+	      String fileName = null;
+	      UUID uuid = UUID.randomUUID();
 		if (file != null) {
-			fileName = UploadFileUtils.fileUpload(uuid, imgUploadPath, file.getOriginalFilename(), file.getBytes(),	ymdPath);
-			log.info("생성하는 파일이름:"+fileName);
+			fileName = UploadFileUtils.fileUpload(uuid, imgUploadPath, file.getOriginalFilename(), file.getBytes(),
+					ymdPath);
 		} else {
-			fileName = root_path+File.separator+uploadPath + File.separator + "images" + File.separator + "none.png";
+			fileName = root_path+uploadPath + File.separator + "images" + File.separator + "none.png";
 		}
 
 		studygroup.setThumbimg(
